fdb8ac7a061d887a88744d2cece3b6498031c93d
Mingyu <minngyuseong@gmail.com>
2026-04-19 19:52:46 +0900

    style: 게시물 상세 헤더 스타일 반영 - 다크모드 반영

--- app/post/_layout.tsx
+++ app/post/_layout.tsx
@@ -1,28 +1,31 @@
-import { Stack, router } from "expo-router"
-import { TouchableOpacity, useColorScheme } from "react-native"
-import { DarkTheme, DefaultTheme } from "@react-navigation/native"
-import ArrowLeftIcon from "@/assets/icons/arrow-left.svg"
+import ArrowLeftIcon from "@/assets/icons/arrow-left.svg";
+import { dark, light } from "@/styles/semantic-colors";
+import { Stack, router } from "expo-router";
+import { TouchableOpacity, useColorScheme } from "react-native";
 
 export default function PostLayout() {
-  const colorScheme = useColorScheme()
+  const scheme = useColorScheme();
+  const colors = scheme === "dark" ? dark : light;
   return (
     <Stack
       screenOptions={{
         headerShown: true,
         headerShadowVisible: false,
         headerStyle: {
-          backgroundColor:
-            colorScheme === "dark" ? DarkTheme.colors.card : DefaultTheme.colors.card,
+          backgroundColor: colors.bg.primary,
         },
         headerLeft: () => (
           <TouchableOpacity onPress={() => router.back()} className="p-2">
-            <ArrowLeftIcon width={24} height={24} />
+            <ArrowLeftIcon width={24} height={24} color={colors.icon.primary} />
           </TouchableOpacity>
         ),
       }}
     >
-      <Stack.Screen name="[id]" options={{ title: "" }} />
-      <Stack.Screen name="create-post" options={{ title: "새 게시물" }} />
+      <Stack.Screen name="[id]" />
+      <Stack.Screen
+        name="create-post"
+        options={{ title: "새 게시물", headerTintColor: colors.text.primary }}
+      />
     </Stack>
-  )
+  );
 }
