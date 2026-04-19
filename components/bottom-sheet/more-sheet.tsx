0fd23c77d7e793c7288724a04974b2467b99a9e3
Mingyu <minngyuseong@gmail.com>
2026-04-19 19:09:37 +0900

    feat: 피드 더보기 bottom sheet UI 완성

---
+++ components/bottom-sheet/more-sheet.tsx
@@ -0,0 +1,39 @@
+import BanIcon from "@/assets/icons/ban.svg";
+import BellIcon from "@/assets/icons/bell.svg";
+import ShareIcon from "@/assets/icons/share.svg";
+import { BaseBottomSheet } from "@/components/bottom-sheet/base-bottom-sheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+
+interface MoreSheetProps {
+  MoreSheetModalRef: React.RefObject<BottomSheetModal | null>;
+}
+
+const MORE_SHEET_ITEMS = [
+  { Icon: BanIcon, label: "신고하기", onPress: () => {} },
+  { Icon: BellIcon, label: "차단하기", onPress: () => {} },
+  { Icon: ShareIcon, label: "공유하기", onPress: () => {} },
+  { Icon: null, label: "집사 프로필 방문하기", onPress: () => {} },
+] as const;
+
+export const MoreSheet = ({ MoreSheetModalRef }: MoreSheetProps) => {
+  return (
+    <BaseBottomSheet BaseBottomSheetModalRef={MoreSheetModalRef}>
+      <View className="flex-1 flex flex-col items-center justify-center pb-16">
+        {MORE_SHEET_ITEMS.map(({ Icon, label, onPress }) => (
+          <Pressable
+            key={label}
+            onPress={onPress}
+            className="flex-row gap-1.5 py-[14px] items-center justify-center active:opacity-60"
+          >
+            {Icon && <Icon />}
+            <Text className="typo-body1 text-semantic-text-primary">
+              {label}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+    </BaseBottomSheet>
+  );
+};
