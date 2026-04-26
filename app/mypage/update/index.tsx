047615aedc2ef25a920053f23b92dd63cc93f8d2
Mingyu <minngyuseong@gmail.com>
2026-04-26 16:04:54 +0900

    feat: 프로필 수정저 구현

--- app/mypage/update/index.tsx
+++ app/mypage/update/index.tsx
@@ -1,76 +1,115 @@
 import { useUserProfileQuery } from "@/api/domains/user/queries";
 import Input from "@/components/common/input";
 import Select from "@/components/common/select";
+import BottomActionBar from "@/components/layout/bottom-action-bar";
 import ProfileImage from "@/components/user/profile-image";
-import { dark, light } from "@/styles/semantic-colors";
 import { Link } from "expo-router";
 import { useState } from "react";
-import {
-  Pressable,
-  ScrollView,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Pressable, ScrollView, Text, View } from "react-native";
 
-const UpdateProfile = () => {
+const UpdateNickname = () => {
   const { data: userData } = useUserProfileQuery();
-
   const [nickname, setNickname] = useState(userData.nickname);
+  return (
+    <View className="flex-col gap-1.5">
+      <Text className="typo-label1 text-semantic-text-primary">이름</Text>
+      <Input
+        value={nickname}
+        onChangeText={setNickname}
+        maxLength={16}
+        placeholder="이름을 입력하세요."
+      />
+    </View>
+  );
+};
+
+const UpdateIfLivingWithCat = () => {
+  const { data: userData } = useUserProfileQuery();
   const [isLivingWithCat, setIsLivingWithCat] = useState(
     userData.isLivingWithCat,
   );
-  const scheme = useColorScheme();
-  const colors = scheme === "dark" ? dark : light;
   return (
-    <SafeAreaView className="flex-1 bg-semantic-bg-primary">
-      <ScrollView className="flex-1">
+    <View className="flex-col gap-1.5">
+      <Text className="typo-label1 text-semantic-text-primary">
+        사용자 유형
+      </Text>
+      <Select
+        options={[
+          { label: "실제 집사", value: true },
+          { label: "랜선 집사", value: false },
+        ]}
+        value={isLivingWithCat}
+        onChange={setIsLivingWithCat}
+      />
+    </View>
+  );
+};
+
+const UpdateTag = () => {
+  const { data: userData } = useUserProfileQuery();
+  const selectedTags = [
+    ...userData.favoriteAppearances,
+    ...userData.favoritePersonalities,
+  ];
+
+  return (
+    <View className="flex-col gap-3">
+      <Text className="typo-label1 text-semantic-text-primary">관심 태그</Text>
+      <View className="flex-row flex-wrap gap-3">
+        {selectedTags.map((tag) => (
+          <Pressable
+            key={tag.id}
+            className="items-center rounded py-1 px-2.5 bg-semantic-chips-primary-bg"
+          >
+            <Text className="typo-body4 text-semantic-chips-primary-text">
+              {tag.label}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <Link href={`/mypage/update/tag`} asChild>
+        <Pressable className="active:opacity-60 w-full">
+          <View className="border border-semantic-border-primary py-2 justify-center items-center rounded">
+            <Text className="typo-body4 text-semantic-text-secondary">
+              태그 편집하기
+            </Text>
+          </View>
+        </Pressable>
+      </Link>
+    </View>
+  );
+};
+
+const UpdateProfile = () => {
+  const { data: userData } = useUserProfileQuery();
+  const [canSubmit, setCanSubmit] = useState(false);
+  return (
+    <View className="flex-1 bg-semantic-bg-primary">
+      <ScrollView
+        className="py-6 px-3"
+        contentContainerStyle={{ flexGrow: 1, alignItems: "center", gap: 40 }}
+      >
         <ProfileImage
           imageUrl={userData.profileImageUrl}
           size="lg"
           isEditMode
         />
-        <View className="w-full flex-col gap-9 px-3">
-          <View className="flex-col gap-1.5">
-            <Text className="typo-label1 text-semantic-text-primary">이름</Text>
-            <Input
-              value={nickname}
-              onChangeText={setNickname}
-              maxLength={16}
-              placeholder="이름을 입력하세요."
-            />
-          </View>
-          <View className="flex-col gap-1.5">
-            <Text className="typo-label1 text-semantic-text-primary">
-              사용자 유형
-            </Text>
-            <Select
-              options={[
-                { label: "실제 집사", value: true },
-                { label: "랜선 집사", value: false },
-              ]}
-              value={isLivingWithCat}
-              onChange={setIsLivingWithCat}
-            />
-          </View>
-          <View className="flex-col gap-1.5">
-            <Text className="typo-label1 text-semantic-text-primary">
-              관심 태그
-            </Text>
-            <Link href={`/mypage/update/tag`} asChild>
-              <Pressable className="active:opacity-60 flex-1">
-                <View className="border border-semantic-border-primary py-2 flex-1 justify-center items-center rounded-sm">
-                  <Text className="typo-body4 text-semantic-text-secondary">
-                    태그 편집하기
-                  </Text>
-                </View>
-              </Pressable>
-            </Link>
-          </View>
+        <View className="w-full flex-col gap-8">
+          <UpdateNickname />
+          <UpdateIfLivingWithCat />
+          <UpdateTag />
         </View>
       </ScrollView>
-    </SafeAreaView>
+      <BottomActionBar
+        buttons={[
+          {
+            label: "저장",
+            onPress: () => {},
+            disabled: !canSubmit, // state 그대로 전달
+          },
+        ]}
+      />
+    </View>
   );
 };
 
