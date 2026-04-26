e95877c62824585dbb66182c2855486db9d4b716
Mingyu <minngyuseong@gmail.com>
2026-04-26 16:01:39 +0900

    fix: user 타입 업데이트 - favoriteAppearances, favoritePersonalities

--- api/domains/user/types.ts
+++ api/domains/user/types.ts
@@ -1,24 +1,32 @@
+import { AttributeItem } from "@/api/domains/attribute/types";
+
 interface Profile {
-  id: string
-  kakaoId: string | null
-  googleId: string | null
-  appleId: string | null
-  isLivingWithCat: boolean
-  hasAgreedToTerms: boolean
-  createdAt: string
-  phone: string | null
-  nickname: string
-  profileImageUrl: string | null
-  followerCount: number
-  followingCount: number
-  isFollowing?: boolean // 팔로우 여부 (선택적 필드)
+  id: string;
+  kakaoId: string | null;
+  googleId: string | null;
+  appleId: string | null;
+  isLivingWithCat: boolean;
+  hasAgreedToTerms: boolean;
+  createdAt: string;
+  phone: string | null;
+  nickname: string;
+  profileImageUrl: string | null;
+  followerCount: number;
+  followingCount: number;
+  isFollowing: boolean; // 팔로우 여부 (선택적 필드)
+  favoriteAppearances: AttributeItem[];
+  favoritePersonalities: AttributeItem[];
 }
 
-type GetUserProfileResponse = Profile
+type GetUserProfileResponse = Omit<Profile, "isFollowing">;
 
 type GetUserByIdResponse = Pick<
   Profile,
-  "followerCount" | "followingCount" | "nickname" | "profileImageUrl" | "isFollowing"
->
+  | "followerCount"
+  | "followingCount"
+  | "nickname"
+  | "profileImageUrl"
+  | "isFollowing"
+>;
 
-export { Profile, GetUserProfileResponse, GetUserByIdResponse }
+export { GetUserByIdResponse, GetUserProfileResponse, Profile };
