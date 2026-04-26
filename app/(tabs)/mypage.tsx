fe73bfb1c9faa9e96b474c67b205c2ba0d9eea7a
Mingyu <minngyuseong@gmail.com>
2026-04-26 16:02:36 +0900

    feat: 마이페이지 일부 구현 - 피드 리스트용 헤더 구현

--- app/(tabs)/mypage.tsx
+++ app/(tabs)/mypage.tsx
@@ -1,28 +1,53 @@
+import ChevronRightIcon from "@/assets/icons/chevron-right.svg";
 import SettingsIcon from "@/assets/icons/settings.svg";
 import CatList from "@/components/cat/cat-list";
-import { Thumbnail } from "@/components/post/thumbnail";
+import CatRegistration from "@/components/cat/cat-registration";
+import IconHeader from "@/components/layout/icon-header";
 import ProfileActions from "@/components/user/my-profile-actions";
 import { ProfileInfo } from "@/components/user/profile-info";
+import { VIEW_TYPE_ORDER, ViewType } from "@/constants/tab";
 import { dark, light } from "@/styles/semantic-colors";
 import { Link, Stack } from "expo-router";
-import { Suspense, useRef, useState } from "react";
-import {
-  ActivityIndicator,
-  FlatList,
-  useColorScheme,
-  useWindowDimensions,
-  View,
-} from "react-native";
+import { useRef, useState } from "react";
+import { Text, useColorScheme, useWindowDimensions, View } from "react-native";
 import { Pressable, ScrollView } from "react-native-gesture-handler";
 import PagerView from "react-native-pager-view";
-import { SafeAreaView } from "react-native-safe-area-context";
+
+const MyCatListSection = () => {
+  const scheme = useColorScheme();
+  const colors = scheme === "dark" ? dark : light;
+  return (
+    <>
+      <View className="w-full flex-row justify-between items-center px-5 mb-3">
+        <Text className="text-semantic-text-secondary typo-body3">
+          함께 사는 고양이
+        </Text>
+        <Link href={`/cat/list`} asChild>
+          <Pressable className="active:opacity-60">
+            <ChevronRightIcon
+              width={16}
+              height={16}
+              color={colors.icon.secondary}
+            />
+          </Pressable>
+        </Link>
+      </View>
+      <View className="flex-row items-center pr-5">
+        <View className="flex-1">
+          <CatList />
+        </View>
+        <CatRegistration />
+      </View>
+    </>
+  );
+};
 
 export default function Mypage() {
   const scheme = useColorScheme();
   const colors = scheme === "dark" ? dark : light;
 
   const pagerRef = useRef<PagerView>(null);
-  const [activeTab, setActiveTab] = useState(0); // 0: my posts, 1: liked posts, 2: bookmarked posts
+  const [activeTab, setActiveTab] = useState<ViewType>("all");
 
   const { width } = useWindowDimensions();
   const ITEM_SIZE = width / 3;
@@ -32,11 +57,13 @@ export default function Mypage() {
     { id: "2", title: "내가 쓴 글 2" },
     { id: "3", title: "내가 쓴 글 3" },
   ];
+
   return (
     <>
       <Stack.Screen
         options={{
           headerShown: true,
+          headerShadowVisible: false,
           title: "마이페이지",
           headerTintColor: colors.text.primary,
           headerStyle: { backgroundColor: colors.bg.primary },
@@ -53,52 +80,27 @@ export default function Mypage() {
           ),
         }}
       />
-      <SafeAreaView className="flex-1 bg-semantic-bg-primary">
-        <ScrollView className="flex-1">
-          <ProfileInfo />
-          <ProfileActions />
-          <CatList />
 
-          <PagerView
-            ref={pagerRef}
-            style={{ flex: 1 }}
-            initialPage={0}
-            onPageSelected={(e) => setActiveTab(e.nativeEvent.position)}
-          >
-            <Suspense
-              fallback={
-                <ActivityIndicator size="large" style={{ marginTop: 50 }} />
-              }
-            >
-              <FlatList
-                data={mockPosts}
-                keyExtractor={(item) => item.id}
-                numColumns={3}
-                ListHeaderComponent={
-                  <>
-                    <ProfileInfo />
-                    <ProfileActions />
-                    <CatList />
-                    {/* 탭 헤더 */}
-                  </>
-                }
-                renderItem={({ item }) => (
-                  <Thumbnail postId={item.id} size={ITEM_SIZE} />
-                )}
-              />
-            </Suspense>
-            <View key="recommended" style={{ flex: 1 }}>
-              <Suspense
-                fallback={
-                  <ActivityIndicator size="large" style={{ marginTop: 50 }} />
-                }
-              >
-                {/* <RecommendedFeedList /> */}
-              </Suspense>
-            </View>
-          </PagerView>
-        </ScrollView>
-      </SafeAreaView>
+      <ScrollView className="flex-1 bg-semantic-bg-primary py-6">
+        <ProfileInfo />
+        <ProfileActions />
+        <MyCatListSection />
+        <View className="h-6" />
+        <IconHeader
+          activeTab={activeTab}
+          onTabChange={(tab) =>
+            pagerRef.current?.setPage(VIEW_TYPE_ORDER.indexOf(tab))
+          }
+        />
+        <PagerView
+          ref={pagerRef}
+          style={{ flex: 1 }}
+          initialPage={0}
+          onPageSelected={(e) =>
+            setActiveTab(VIEW_TYPE_ORDER[e.nativeEvent.position])
+          }
+        ></PagerView>
+      </ScrollView>
     </>
   );
 }
