755b10c176e0f08b59dcd4c4bdf056b69563f223
Mingyu <minngyuseong@gmail.com>
2026-04-28 01:02:29 +0900

    feat: checkbox 컴포넌트 구현 - 체크여부 상태, 콜백, 라벨 prop

---
+++ components/common/checkbox.tsx
@@ -0,0 +1,27 @@
+import CheckboxFilled from "@/assets/icons/checkbox-filled.svg";
+import CheckboxOutline from "@/assets/icons/checkbox-outline.svg";
+import { dark, light } from "@/styles/semantic-colors";
+import { Pressable, Text, useColorScheme } from "react-native";
+
+interface CheckboxProps {
+  isChecked: boolean;
+  onToggle: () => void;
+  label: string;
+}
+
+const Checkbox = ({ isChecked, onToggle, label }: CheckboxProps) => {
+  const scheme = useColorScheme();
+  const colors = scheme === "dark" ? dark : light;
+  return (
+    <Pressable className="flex-row items-center gap-1.5" onPress={onToggle}>
+      {isChecked ? (
+        <CheckboxFilled width={24} height={24} />
+      ) : (
+        <CheckboxOutline width={24} height={24} color={colors.icon.secondary} />
+      )}
+      <Text className="typo-body1 text-semantic-text-primary">{label}</Text>
+    </Pressable>
+  );
+};
+
+export default Checkbox;
