f18286f1af53f3d94cf57291ce607a15e41f3146
Mingyu <minngyuseong@gmail.com>
2026-04-28 02:00:48 +0900

    feat: 온보딩 - 집사여부 화면 구현

--- app/(auth)/(onboarding)/step2.tsx
+++ app/(auth)/(onboarding)/step2.tsx
@@ -1,51 +1,46 @@
-import OnboardingWrapper from "@/components/OnboardingWrapper";
-import { cn } from "@/lib/utils";
-import { useOnboardingStore } from "@/store/useOnboardingStore";
-import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import Select from "@/components/common/select";
+import BottomActionBar from "@/components/layout/bottom-action-bar";
+import ProgressBar from "@/components/onboarding/progress-bar";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Step2() {
-  const router = useRouter();
-  const { isCatOwner, setIsCatOwner } = useOnboardingStore();
-
-  const handleNext = () => {
-    router.push("/(auth)/(onboarding)/step3");
-  };
+  const [isLivingWithCat, setIsLivingWithCat] = useState(
+    null as null | boolean,
+  );
 
   return (
-    <OnboardingWrapper
-      step={2}
-      title="지금 고양이와 살고 있나요?"
-      onNext={handleNext}
-      isNextEnabled={isCatOwner !== null}
-    >
-      <View className="flex-row gap-3">
-        {/* '네' 버튼 */}
-        <TouchableOpacity
-          onPress={() => setIsCatOwner(true)}
-          className={cn(
-            "flex-1 items-center rounded-xl py-4",
-            isCatOwner === true
-              ? "bg-semantic-button-primary-background"
-              : "bg-semantic-bg-secondary",
-          )}
-        >
-          <Text className="text-body1 font-bold">네</Text>
-        </TouchableOpacity>
-
-        {/* '아니오' 버튼 */}
-        <TouchableOpacity
-          onPress={() => setIsCatOwner(false)}
-          className={cn(
-            "flex-1 items-center rounded-xl py-4",
-            isCatOwner === false
-              ? "bg-semantic-button-primary-background"
-              : "bg-semantic-bg-secondary",
-          )}
-        >
-          <Text className="text-body1 font-bold">아니오</Text>
-        </TouchableOpacity>
-      </View>
-    </OnboardingWrapper>
+    <View className="flex-1 bg-semantic-bg-primary">
+      <ScrollView
+        className="py-10 px-5"
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <ProgressBar progress={2} />
+        <View className="h-10" />
+        <Text className="typo-title2 text-semantic-text-primary">
+          지금 고양이와 살고 있나요?
+        </Text>
+        <View className="h-6" />
+        <Select
+          options={[
+            { label: "네", value: true },
+            { label: "아니오", value: false },
+          ]}
+          value={isLivingWithCat}
+          onChange={setIsLivingWithCat}
+        />
+      </ScrollView>
+      <BottomActionBar
+        buttons={[
+          {
+            label: "다음으로",
+            onPress: () => router.push("/(auth)/(onboarding)/step3"),
+            disabled: isLivingWithCat === null,
+          },
+        ]}
+      />
+    </View>
   );
 }
