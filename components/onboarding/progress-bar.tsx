c813ee7caa3269c7093fefbaa11c437ed66f1d88
Mingyu <minngyuseong@gmail.com>
2026-05-05 21:23:20 +0900

    feat: progress-bar에 gradient 적용

--- components/onboarding/progress-bar.tsx
+++ components/onboarding/progress-bar.tsx
@@ -1,25 +1,32 @@
-import { dark, light } from "@/styles/semantic-colors";
-import { useColorScheme, View } from "react-native";
+import Gradient from "@/components/common/gradient";
+import { useColors } from "@/hooks/use-colors";
+import { View } from "react-native";
+const TOTAL_STEPS = 7;
 
 const ProgressBar = ({ progress }: { progress: number }) => {
-  const scheme = useColorScheme();
-  const colors = scheme === "dark" ? dark : light;
+  const { colors } = useColors();
 
-  const TOTAL_STEPS = 7;
   return (
-    <View className="w-full flex-row gap-1.5">
-      {[...Array(TOTAL_STEPS)].map((_, index) => (
-        <View
-          key={index}
-          className="h-2 rounded-sm flex-1"
-          style={{
-            backgroundColor:
-              index < progress ? colors.icon.accent : colors.bg.secondary,
-          }}
-        />
-      ))}
+    <View className="relative">
+      <View className="px-5 flex-row gap-1.5">
+        {[...Array(TOTAL_STEPS)].map((_, index) => (
+          <View
+            key={index}
+            className="h-2 rounded-sm flex-1"
+            style={{
+              backgroundColor:
+                index < progress ? colors.icon.accent : colors.bg.secondary,
+            }}
+          />
+        ))}
+      </View>
+      <Gradient
+        direction="vertical"
+        width="100%"
+        height={20}
+        style={{ position: "absolute", bottom: -20, left: 0, zIndex: 10 }}
+      />
     </View>
   );
 };
-
 export default ProgressBar;
