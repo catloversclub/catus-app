343fd49214eda74bd9f02941b0d2a5b02f3de4d4
Mingyu <minngyuseong@gmail.com>
2026-06-22 18:56:55 +0900

    fix: 공유 링크 베이스 URL 수정

--- lib/share.ts
+++ lib/share.ts
@@ -1,23 +1,23 @@
-import { WEBVIEW_URL } from "@/constants/api";
+import { API_BASE_URL } from "@/constants/api";
 import { Share } from "react-native";
 
 const shareUser = async (userId: string, nickname: string) => {
   await Share.share({
-    url: `${WEBVIEW_URL}/share/user/${userId}`,
+    url: `${API_BASE_URL}/share/user/${userId}`,
     message: `${nickname}님의 프로필을 확인해보세요!`,
   });
 };
 
 const sharePost = async (postId: string, nickname: string) => {
   await Share.share({
-    url: `${WEBVIEW_URL}/share/post/${postId}`,
+    url: `${API_BASE_URL}/share/post/${postId}`,
     message: `${nickname}님의 게시물을 확인해보세요!`,
   });
 };
 
 const shareCat = async (catId: string, catName: string) => {
   await Share.share({
-    url: `${WEBVIEW_URL}/share/cat/${catId}`,
+    url: `${API_BASE_URL}/share/cat/${catId}`,
     message: `${catName}의 프로필을 확인해보세요!`,
   });
 };
